110d48d127a95c5ee5836198ffe447183c03c31e
Mikola3 <34712864+Mikola3@users.noreply.github.com>
2021-03-03 20:20:23 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew cask install visual-studio-code
-brew cask install iterm2
-brew cask install google-chrome
-brew cask install postman
+# correct way to install brew
+echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install --cask visual-studio-code
+brew install --cask iterm2
+brew install --cask google-chrome
+brew install --cask postman
+brew install --cask zoom
 brew install unrar
 # unrar
 # unrar e your.rar
 
-brew cask install skype
-brew cask install slack
-brew cask install microsoft-teams
+brew install --cask skype
+brew install --cask slack
+brew install --cask telegram
+brew install --cask evernote
+brew install --cask anki
 
-brew cask install evernote
-brew cask install anki
-
-brew cask install --force virtualbox
+brew install --cask --force virtualbox
 # overcome the issue with installing virtualbox
 # https://github.com/Homebrew/homebrew-cask/issues/39369
-brew cask install vagrant
-brew cask install docker
-brew cask install vlc
+brew install --cask vagrant
+brew install --cask docker
+brew install --cask vlc
 
 brew install tig
 brew install jq
@@ -43,7 +44,9 @@ brew install vault
 brew install bash-completion
 brew install shellcheck
 
-brew install awscli
+# AWS Cli ver. 2
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target /
 # aws authenticator
 sudo chown -R $(whoami) /usr/local/lib/pkgconfig
 brew install aws-iam-authenticator
@@ -53,35 +56,37 @@ brew install minikube
 brew install pandoc
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # after installing, add "plugins=(zsh-autosuggestions)" in ~/.zshrc
+# To resolve the issue with oh-my-zsh https://github.com/ohmyzsh/ohmyzsh/issues/6835
+chmod 755 /usr/local/share/zsh
+chmod 755 /usr/local/share/zsh/site-functions
 
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-~/.pyenv/bin/pyenv install 3.7.4
-~/.pyenv/bin/pyenv global 3.7.4
 
-~/.pyenv/versions/3.7.4/bin/pip3 install --upgrade pip
-# ~/.pyenv/versions/3.7.4/bin/pip3 install awscli
-~/.pyenv/versions/3.7.4/bin/pip3 install boto3
-~/.pyenv/versions/3.7.4/bin/pip3 install ansible
-/usr/local/opt/python/bin/python3.7 -m pip install -U pylint --user
+# Install python related packages
+brew install python3
 
-python3 -m pip install powerline-status
+pip3 install --upgrade pip
+pip3 install boto3
+pip3 install ansible
+pip3 install pylint
+pip3 install autopep
+pip3 install powerline-status
 # Needed additional configuration for powerline https://medium.com/@ITZDERR/how-to-install-powerline-to-pimp-your-bash-prompt-for-mac-9b82b03b1c02
+
+# python3 -m pip install -U pylint --user
 
 code --install-extension ms-python.python
 code --install-extension equinusocio.vsc-material-theme
-code --install-extension vscoss.vscode-ansible
 code --install-extension davidanson.vscode-markdownlint
 code --install-extension eamodio.gitlens
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
-# autocomplete most pr. langs (intellect)
+# autocomplete for the most programming langs (intellect)
 code --install-extension tabnine.tabnine-vscode
 
-python3 -m pip install autopep8
 code --install-extension himanoa.python-autopep8
 code --install-extension vscoss.vscode-ansible
 code --install-extension davidanson.vscode-markdownlint
@@ -90,5 +95,14 @@ code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 code --install-extension amazonwebservices.aws-toolkit-vscode
 code --install-extension timonwong.shellcheck
 
+# yaml format when saving the yaml file: "Format Document" command in VSC
+code --install-extension redhat.vscode-yaml
+
 # Disable your last log in to system
 touch ~/.hushlogin
+
+# Increase the speed of kursor
+# Keyboard -> 'Key Repeat' -> 'Fast' and 'Delay Until Repeat' -> 'Slow'
+
+# Avoid the issue with iterm after typing 'Option + <'
+# https://apple.stackexchange.com/questions/136928/using-alt-cmd-right-left-arrow-in-iterm
